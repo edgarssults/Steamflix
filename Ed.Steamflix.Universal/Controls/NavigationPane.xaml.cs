533f8a47bfeb8754138a82ce188f7763a612e030
Edgars Šults <edgars.sults@gmail.com>
2016-11-10 22:39:22 +0200

    Added simple hamburger menu

---
+++ Ed.Steamflix.Universal/Controls/NavigationPane.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace Ed.Steamflix.Universal
+{
+    public sealed partial class NavigationPane : UserControl
+    {
+        public NavigationPane()
+        {
+            this.InitializeComponent();
+        }
+
+        private void HamburgerButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationView.IsPaneOpen = !NavigationView.IsPaneOpen;
+            NavigationView.Width = NavigationView.IsPaneOpen ? NavigationView.OpenPaneLength : NavigationView.CompactPaneLength;
+        }
+
+        private void NavigateToGames(object sender, RoutedEventArgs e)
+        {
+            ((Frame)Window.Current.Content).Navigate(typeof(GamesPage));
+        }
+    }
+}
